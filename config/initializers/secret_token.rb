ef178d94c627e41a87e73d383b00d2f9eb055b9c
picho <picho.ep@gmail.com>
2014-04-08 08:49:06 -0430

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mascotas::Application.config.secret_key_base = '7a03682bf36cafc340103eb7a737c35dc17b8d80af73d48b41eb6ab19a81d80c1c95a5ec05a91d138a4734883d53e7690a4c2cb31744c3c7367ce1c5ec172179'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mascotas::Application.config.session_store :cookie_store, key: '_Mascotas_session'
